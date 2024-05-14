39e9cfeaa5cb211ff7a48c2ef7d6975f7e68912b
Umair Ul Hassan <mrumairhassan@outlook.com>
2024-05-14 12:21:38 +0500

    Bulk data added
    More than one data

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,8 +33,15 @@ var getUser = async (req,res) =>{
 //post users
 var postUsers = async (req, res) => {
     try {
+
+        
         var postData = req.body;
-        var data = await User.create(postData);
+        if (postData.length> 1){  // if more than one data (array handle )
+          var data = await User.bulkCreate(postData); // for bulk data
+        }else {
+            var data = await User.create(postData);
+        }
+        
         res.status(200).json({ data: data });
     } catch (error) {
         console.error('Error creating user:', error);
